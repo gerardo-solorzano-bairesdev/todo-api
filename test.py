d26763da3c48fa122c1cf0fc69995e07f54ad40b
Guilherme Rodrigues <gadr90@gmail.com>
2015-04-12 18:55:33 -0300

    Decode data as utf8

--- test.py
+++ test.py
@@ -10,20 +10,20 @@ class APITestCase(unittest.TestCase):
 
     def test_hello_world(self):
         rv = self.app.get('/')
-        assert "Hej, världen!" in rv.data
+        assert "Hej, världen!" in rv.data.decode('utf8')
 
     def test_default_todos(self):
         rv = self.app.get('/api/todos/')
         with open('fixtures.json') as todos_file:
             expected_todos = json.load(todos_file)
-        api_todos = json.loads(rv.data)['todos']
+        api_todos = json.loads(rv.data.decode('utf8'))['todos']
         assert expected_todos == api_todos
 
     def test_get_todo(self):
         rv = self.app.get('/api/todos/1')
         with open('fixtures.json') as todos_file:
             expected_todo = json.load(todos_file)[0]
-        api_todo = json.loads(rv.data)
+        api_todo = json.loads(rv.data.decode('utf8'))
         assert expected_todo == api_todo
 
     # TODO: Stop depending on test case execution order
@@ -32,7 +32,7 @@ class APITestCase(unittest.TestCase):
         rv = self.app.post('/api/todos/',
                            data=json.dumps(data),
                            content_type='application/json')
-        api_todo = json.loads(rv.data)
+        api_todo = json.loads(rv.data.decode('utf8'))
         assert 'Test' == api_todo['title']
 
     def test_put_todo(self):
@@ -40,12 +40,12 @@ class APITestCase(unittest.TestCase):
         rv = self.app.put('/api/todos/3',
                           data=json.dumps(data),
                           content_type='application/json')
-        api_todo = json.loads(rv.data)
+        api_todo = json.loads(rv.data.decode('utf8'))
         assert 'Test2' == api_todo['title']
 
     def test_remove_todo(self):
         rv = self.app.delete('/api/todos/3')
-        assert 'OK' == rv.data
+        assert 'OK' == rv.data.decode('utf8')
 
 
 if __name__ == '__main__':
